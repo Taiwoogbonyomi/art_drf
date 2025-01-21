b3a978f3cc3662a0447563fd1e9eb92c3d6fd5cf
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2025-01-21 15:59:24 +0000

    Added PostList view to list and create a post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    
+
+    def get(self, request):
+        posts = Post.objects.all()
+        title_filter = request.query_params.get('title', None)
+        if title_filter:
+            posts = posts.filter(title__icontains=title_filter)
+        posts = posts.order_by('-created_at')
+        serializer = PostSerializer(posts, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            try:
+                serializer.save(owner=request.user)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response(
+                {"error": str(e)}, 
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+

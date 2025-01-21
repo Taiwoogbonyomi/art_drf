ee36e8e7a1ab36539e75eb64274d4835cf124883
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2025-01-21 12:19:58 +0000

    Created posts model,Added image filter, choice field and image validation

---
+++ posts/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    """
+    Post model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    image_filter_choices = [
+        ('oil_painting', 'Oil Painting'),
+        ('acrylic', 'Acrylic'),
+        ('watercolor', 'Watercolor'),
+        ('impressionist', 'Impressionist'),
+        ('abstract_art', 'Abstract Art'),
+        ('pencil_sketch', 'Pencil Sketch'),
+        ('cubism', 'Cubism'),
+        ('pop_art', 'Pop Art'),
+        ('surrealism', 'Surrealism'),
+        ('expressionism', 'Expressionism'),
+        ('ink_wash', 'Ink Wash')
+    ]
+    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, default="No content provided.")
+    image = models.ImageField(
+        upload_to='images/', 
+        default='../default_post_tx8nvq', 
+        blank=True
+    )
+    image_filter = models.CharField(
+        max_length=32, 
+        choices=image_filter_choices, 
+        default='oil_painting'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        return f'{self.id} {self.title[:30]}...'

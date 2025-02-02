589525d2ea244e3e584d0a08ef3136d351032a4c
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2025-02-02 14:41:57 +0000

    Added register view in urls

--- art_drf/urls.py
+++ art_drf/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from dj_rest_auth.registration.views import RegisterView
 from .views import root_route, logout_route
 
 urlpatterns = [
@@ -23,6 +24,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     
     # Authentication routes
+    path('dj-rest-auth/registration/', RegisterView.as_view(), name='rest_register'),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),

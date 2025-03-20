abeecb3eda9102e4d7bb96aa56f8dc078e82fd7a
Taiwoogbonyomi <taiwoogbonyomi@gmail.com>
2025-03-20 16:11:56 +0000

    Set debug in os.environ

--- art_drf/settings.py
+++ art_drf/settings.py
@@ -69,7 +69,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
